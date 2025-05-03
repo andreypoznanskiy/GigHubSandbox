e64a06de776e1cb0b29921c3879691ad048d89e4
Andrii Poznanskyi <andrii.poznanskyi@lynceetec.com>
2025-05-03 22:01:56 +0300

    Add create gigs page

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        public ActionResult Create()
+        {
+            return new ViewResult();
+        }
+    } 
+}
