f343b7923470c014ccd7c9d297559dd4b3348065
Andrii Poznanskyi <andrii.poznanskyi@lynceetec.com>
2025-05-05 21:44:02 +0300

    Create Add a Gig form

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,13 +1,27 @@
-﻿using System.Web.Mvc;
-using System.Web.UI.WebControls;
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Create()
         {
-            return new ViewResult();
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
         }
     } 
 }
