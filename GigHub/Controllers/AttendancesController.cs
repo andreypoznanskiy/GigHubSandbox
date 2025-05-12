8f46456c24d44f84d61fc50e8bd7252a41d24df6
Andrii Poznanskyi <andrii.poznanskyi@lynceetec.com>
2025-05-12 22:02:54 +0300

    Add going to gig functionality

--- GigHub/Controllers/AttendancesController.cs
+++ GigHub/Controllers/AttendancesController.cs
@@ -1,32 +1,47 @@
-﻿//using GigHub.Models;
-//using Microsoft.AspNet.Identity;
-//using System.Web.Http;
+﻿using System.Linq;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http;
 
-//namespace GigHub.Controllers
-//{
-//    [Authorize]
-//    public class AttendanceController : ApiController
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace GigHub.Controllers
+{
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
 
-//        public AttendanceController()
-//        {
-//            _context = new ApplicationDbContext();
-//        }
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
 
-//        [HttpPost]
-//        public IHttpActionResult Attend([FromBody] int gigId)
-//        {
-//            var attendance = new Attendance
-//            {
-//                GigId = gigId,
-//                AttendeeId = User.Identity.GetUserId()
-//            };
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
 
-//            _context.Attendances.Add(attendance);
-//            _context.SaveChanges();
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            var exists = _context.Attendances
+                .Any(a => a.GigId == attendanceDto.GigId && a.AttendeeId == userId);
 
-//            return Ok();
-//        }
-//    }
-//}
+            if (exists)
+            {
+                return BadRequest("The attendance already exists.");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = attendanceDto.GigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- GigHub/Models/IdentityModels.cs
+++ GigHub/Models/IdentityModels.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +12,19 @@ namespace GigHub.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public ICollection<Following> Followers { get; set; }
+        public ICollection<Following> Followees { get; set; }
+
+        public ApplicationUser()
+        {
+            Followers = new Collection<Following>();
+            Followees = new Collection<Following>();
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -23,6 +39,7 @@ namespace GigHub.Models
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -39,6 +56,16 @@ namespace GigHub.Models
             modelBuilder.Entity<Attendance>()
                 .HasRequired(a => a.Gig)
                 .WithMany()
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
                 .WillCascadeOnDelete(false);
 
             base.OnModelCreating(modelBuilder);
